c5da04687ead0e9998449eceb6bc61c83cdf1e27
Kiichi Ueta <Kiichi@k-ueta.(none)>
2017-05-11 00:36:40 +0900

    updated

--- horse_race_service.py
+++ horse_race_service.py
@@ -1,20 +1,23 @@
 import numpy as np
 from dao.input_dao import InputDao
+from dao.output_dao import OutputDao
 from transform.scale.boolean_scaler import BooleanScaler
 from transform.transform import Transform
 from utils.output_writer import OutputWriter
 from validation.cross_validation import CrossValidation
 from search.grid_search import GridSearch
 from executor.parallel import Parallel
-
+from learn.classification.svm import SVM
+from analytics import Analytics
 
 class HorseRaceService:
 
     output_file = 'horse_race_result.csv'
     spark_app_name = 'Horse Race Analysis'
 
-    def __init__(self, cross_validation_config):
-        self.dao = InputDao()
+    def __init__(self):
+        self.input_dao = InputDao()
+        self.output_dao = OutputDao()
         self.features = [
             'DISTANCE',
             'WEATHERCD',
@@ -52,15 +55,45 @@ class HorseRaceService:
         self.scale_types = {
             'CONFTYAKU': BooleanScaler('01')
         }
-        self.cross_validation_config = cross_validation_config
+        self.hyper_parameter_values = {
+            "c": {
+                "from": 2 ^ (-5),
+                "to": 2 ^ 5,
+                "unit": 2
+            },
+            "gamma": {
+                "from": 2 ^ (-5),
+                "to": 2 ^ 5,
+                "unit": 2
+            }
+        }
+        self.outputs = {
+            "c":"numeric(10,10)",
+            "gamma":"numeric(10,10)",
+            "sale":"numeric(10,10)",
+            "expense": "numeric(10,10)",
+            "profit": "numeric(10,10)"
+        }
+        self.k_fold = 2
 
     def execute(self):
         pass
 
-    def test(self, k_fold):
-        data = self.dao.read_data_as_pdf()
+    def test(self):
+        data = self.input_dao.read_data_as_pdf()
+        self.output_dao.init_table(self.outputs)
         transformed_data = Transform(self.valid_ranges, self.scale_types).execute(data)
-        GridSearch.svm(CrossValidation(k_fold), self.features, self.answer, transformed_data, self.write_score, Parallel(self.spark_app_name))
+        Analytics.start(
+            GridSearch(),
+            Parallel(self.spark_app_name),
+            SVM(),
+            CrossValidation(self.k_fold),
+            self.hyper_parameter_values,
+            self.features,
+            self.answer,
+            transformed_data,
+            self.write_score
+        )
 
     @classmethod
     def write_score(cls, answer, expect, data, hyper_parameters):
@@ -75,7 +108,7 @@ class HorseRaceService:
         row['expense'] = expense
         row['profit'] = sale - expense
         print('result=' + str(row))
-        OutputWriter.dict_to_csv(cls.output_file, field_names, row)
+        OutputDao().write_data(row)
 
 if __name__ == '__main__':
-    HorseRaceService(None).test(2)
+    HorseRaceService().test()
